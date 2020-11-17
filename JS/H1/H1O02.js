317c4a648b2cc4550bed97d8faa9cf8cb19b1fba
SteijnMulder <152376@csgaugustinus.nl>
2020-11-17 16:10:23 +0000

    k

--- JS/H1/H1O02.js
+++ JS/H1/H1O02.js
@@ -1,13 +1,14 @@
 function setup() {
   canvas = createCanvas(450,450);
-  background('orange');
+  background('darkred');
   canvas.parent('processing');
   noLoop();
 }
 
 function draw() {
   noStroke();
-  fill('darkred');
-  triangle(225,0,0,450,450,450);
-  triangle(225,0,0,450,450,450);
+  fill('pink');
+  triangle(0,225,450,0,450,450);
+  fill('purple');
+  triangle(450,225,0,0,0,450);
 }
